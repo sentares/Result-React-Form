fecad51b14611518397ec2c3054833f018490052
sentares <aydararzybekov2002@mail.ru>
2025-05-21 15:45:57 +0600

    feat(classNames): create the function to work with classNames

---
+++ src/core/helpers/classNames/classNames.ts
@@ -0,0 +1,17 @@
+export interface Mods {
+	[className: string]: boolean | string | undefined
+}
+
+export function classNames(
+	cls: string,
+	mods: Mods = {},
+	additional: string[] = []
+): string {
+	return [
+		cls,
+		...additional.filter(Boolean),
+		...Object.entries(mods)
+			.filter(([_, value]) => Boolean(value))
+			.map(([className]) => className),
+	].join(' ')
+}
