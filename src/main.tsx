24a6126e3b91cd936c3d74f24bb6ba4747031172
sentares <aydararzybekov2002@mail.ru>
2025-05-21 15:44:39 +0600

    chore: update configs, add styles and init scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './app/styles/index.scss'
+import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
