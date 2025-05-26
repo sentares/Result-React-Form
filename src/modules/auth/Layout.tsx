5e706e74df8655d936048bcd1b78a103643c49ca
sentares <aydararzybekov2002@mail.ru>
2025-05-26 10:01:44 +0600

    feat(Layout): update LayoutAuth

--- src/modules/auth/Layout.tsx
+++ src/modules/auth/Layout.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import cls from './Layout.module.scss'
 import Login from './login/Login'
+import Register from './register/Register'
 
 function LayoutAuth() {
-	const [activeAuth, setActriveAuth] = useState<'login' | 'register'>('login')
+	const [activeAuth, setActiveAuth] = useState<'login' | 'register'>('login')
 
-	const handleChangeActiveAuth = () => {}
+	const toggleAuth = () => {
+		setActiveAuth(prev => (prev === 'login' ? 'register' : 'login'))
+	}
 
 	return (
 		<div className={cls.Layout}>
-			<Login onSubmit={handleChangeActiveAuth} />
+			{activeAuth === 'login' ? (
+				<Login onSubmit={() => {}} onSwitch={toggleAuth} />
+			) : (
+				<Register onSubmit={() => {}} onSwitch={toggleAuth} />
+			)}
 		</div>
 	)
 }
