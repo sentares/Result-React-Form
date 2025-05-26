fe3929a3f309ce20c8259c5c9b8cf59c6c3ed583
sentares <aydararzybekov2002@mail.ru>
2025-05-26 10:29:53 +0600

    feat(Layout): add onSubmit functions

--- src/modules/auth/Layout.tsx
+++ src/modules/auth/Layout.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import cls from './Layout.module.scss'
-import Login from './login/Login'
-import Register from './register/Register'
+import Login, { type LoginOptions } from './login/Login'
+import Register, { type RegisterOptions } from './register/Register'
 
 function LayoutAuth() {
 	const [activeAuth, setActiveAuth] = useState<'login' | 'register'>('login')
@@ -10,12 +10,22 @@ function LayoutAuth() {
 		setActiveAuth(prev => (prev === 'login' ? 'register' : 'login'))
 	}
 
+	const onSubmitLogin = (data: LoginOptions) => {
+		alert(`Добро пожаловать, ${data.email}!`)
+		toggleAuth()
+	}
+
+	const onSubmitRegister = (data: RegisterOptions) => {
+		alert(`Добро пожаловать, ${data.name}!`)
+		toggleAuth()
+	}
+
 	return (
 		<div className={cls.Layout}>
 			{activeAuth === 'login' ? (
-				<Login onSubmit={() => {}} onSwitch={toggleAuth} />
+				<Login onSubmit={onSubmitLogin} onSwitch={toggleAuth} />
 			) : (
-				<Register onSubmit={() => {}} onSwitch={toggleAuth} />
+				<Register onSubmit={onSubmitRegister} onSwitch={toggleAuth} />
 			)}
 		</div>
 	)
